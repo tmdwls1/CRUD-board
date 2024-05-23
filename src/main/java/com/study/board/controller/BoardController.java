74de8a6f3d11412b77d261444e8b962c7f26f125
kim <ken0713@naver.com>
2024-05-24 01:39:08 +0900

    게시글 수정

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,6 +47,24 @@ public class BoardController {
     public String boardDelete(Integer id){
         boardService.boardDelete(id);
         return "redirect:/board/list";
+    }
+    //게시글 수정 폼
+    @GetMapping("/board/modify/{id}")
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardmodify";
+    }
+
+    //게시글 수정 처리
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+        //기존의 board 에 수정된 데이터를 받아와 덮어씌움
+        Board boardTemp = boardService.boardView(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+        boardService.write(boardTemp);
+        return "redirect:/board/list";
+
     }
 
 }
